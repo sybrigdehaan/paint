15726bdd2b0c34896949e4775eaa4639c022f11f
Sybrig <haan1712@student.nhl.nl>
2019-03-12 14:13:27 +0100

    update ornament

---
+++ WPF/paint/paint/Code/Ornament.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paint
+{
+    public abstract class Ornament 
+    {
+        //var color = Color.FromArgb(0, 0, 0, 0);
+        //TextBlock textBlock = new TextBlock();
+        //textBlock.Text = "ornament"; 
+        
+    }
+
+    public class Left : Ornament
+    {
+        
+    }
+
+    public class Right : Ornament
+    {
+
+    }
+
+    public class Top : Ornament
+    {
+
+    }
+
+    public class Bottom : Ornament
+    {
+
+    }
+
+}
