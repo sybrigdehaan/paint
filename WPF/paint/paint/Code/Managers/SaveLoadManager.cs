246e70bcaf1a51465e92c8b740f66ab62d849902
Robin Hogerhuis <robin@mbp-van-robin.access.nhl.nl>
2019-04-18 16:12:43 +0200

    load and save still not working

--- WPF/paint/paint/Code/Managers/SaveLoadManager.cs
+++ WPF/paint/paint/Code/Managers/SaveLoadManager.cs
@@ -32,7 +32,10 @@ namespace paint
             _Shape _myShape = null; 
             SimpleRemoteControl remote = null;
             FrameworkElement myShape = null; 
-            int maxJump = 0; 
+            int maxJump = 0;
+            bool reachedMax = false; 
+            double top = 0;
+            double left = 0; 
 
             List<IFigures> subFigures = new List<IFigures>(); 
             foreach (IFigures figure in MyMainGroup.GetInstance().SubFigures.ToList())
@@ -50,16 +53,26 @@ namespace paint
 
             for (int i = maxJump; i > 0 ; i--)
             {
-                 _Group group = new _Group();
-                
-                foreach (string line in lines)
+                _Group group = null;
+                List<IFigures> inSubFigures = new List<IFigures>();
+
+                for (int j = 0; j < lines.Count; j++)
                 {
-                    int countJump = line.TakeWhile(c => c == '-').Count();
+                    int countJump = lines[j].TakeWhile(c => c == '-').Count();
+                    string[] entries = lines[j].Split(' ');
+                    string nameObject = entries[1];
+
+                    if (countJump == (i - 1) && nameObject == "Group")
+                    {
+                        group = new _Group();
+                        left = Convert.ToDouble(entries[2]);
+                        top = Convert.ToDouble(entries[3]);
+                        reachedMax = false; 
+                    }
+
                     if (countJump == i)
                     {
-                        string[] entries = line.Split(' ');
-                        string nameObject = entries[1];
-
+                        reachedMax = true; 
                         if (nameObject == "Rectangle") {
                              _myShape = new _Rectangle(); }
 
@@ -70,26 +83,42 @@ namespace paint
                         {
                             remote = new SimpleRemoteControl { SetCommand = new _MakeShape(_myShape) };
                             myShape = _myShape.GetShape();
-                            subFigures.Add(_myShape);
+                            inSubFigures.Add(_myShape);
                             _myShape.SetColor();
 
-                            InkCanvas.SetLeft(myShape, Convert.ToDouble(entries[2]));
-                            InkCanvas.SetTop(myShape, Convert.ToDouble(entries[3]));
+                            InkCanvas.SetLeft(myShape, Convert.ToDouble(entries[2]) + left);
+                            InkCanvas.SetTop(myShape, Convert.ToDouble(entries[3]) + top);
                             myShape.Width = Convert.ToDouble(entries[4]);
                             myShape.Height = Convert.ToDouble(entries[5]);
                             remote.buttonWasPressed();
                         }
                     }
+
+                    int nextCountJump;
+                    try { nextCountJump = lines[j + 1].TakeWhile(c => c == '-').Count(); } catch { nextCountJump = -1; }
+                    if (nextCountJump != i && reachedMax && (i - 1 != 0))
+                    {
+                        remote = new SimpleRemoteControl { SetCommand = new _EnGroup(group, inSubFigures) };
+                        remote.buttonWasPressed();
+                        inSubFigures = new List<IFigures>();
+                        subFigures.Add(group);
+                    }
                 }
 
-                if ((i - 1 ) != 0)
-                {
-                    remote = new SimpleRemoteControl { SetCommand = new _EnGroup(group, subFigures) };
-                    remote.buttonWasPressed();
-                    subFigures = new List<IFigures>();
-                    subFigures.Add(group);
-                }
+                //if ((i - 1) != 0 && subFigures.Count != 1 && subFigures.Count != 0)
+                //{
+                //    //group = new _Group();
+                //    //remote = new SimpleRemoteControl { SetCommand = new _EnGroup(group, subFigures) };
+                //    //remote.buttonWasPressed();
+                //    //subFigures = new List<IFigures>();
+                //    //subFigures.Add(group);
+                //}
             }
+            //foreach (IFigures figure in subFigures)
+            //{
+            //    MyMainGroup.GetInstance().Add(figure);
+            //}
+            
         }
     }
 }
