404427e8adb76d4d6ed848a2d36e81b00595e449
Sybrig <haan1712@student.nhl.nl>
2019-04-21 19:06:35 +0200

    Merge, nieuweste versie

---
+++ WPF/paint/paint/Interfaces/IVisitors/IFugures.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace paint
+{
+    public interface IFigures
+    {
+        FrameworkElement GetShape();
+        int GetDepthInList();
+        void DepthInList(int depthInList);
+
+        //Commands 
+        void Make();
+        void Destroy();
+
+        //Visitor
+        void Accept(IWriteToFileVisitor visitor);
+
+    }
+}
